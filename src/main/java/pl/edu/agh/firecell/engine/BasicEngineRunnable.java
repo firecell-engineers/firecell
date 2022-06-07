15435a45be3de0cabdf130b1a2bd6e4f717dcd54
Kamil <61463685+K4m1lP@users.noreply.github.com>
2022-06-07 19:49:09 +0200

    Some basic logic, need to improvement in future.

--- src/main/java/pl/edu/agh/firecell/engine/BasicEngineRunnable.java
+++ src/main/java/pl/edu/agh/firecell/engine/BasicEngineRunnable.java
@@ -3,6 +3,8 @@ package pl.edu.agh.firecell.engine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.edu.agh.firecell.engine.algorithm.Algorithm;
+import pl.edu.agh.firecell.model.Cell;
+import pl.edu.agh.firecell.model.Index;
 import pl.edu.agh.firecell.model.State;
 import pl.edu.agh.firecell.storage.Storage;
 
@@ -12,8 +14,8 @@ public class BasicEngineRunnable implements Runnable {
 
     private State currentState;
     private int currentStateIndex;
-    private Storage storage;
-    private Algorithm algorithm;
+    private final Storage storage;
+    private final Algorithm algorithm;
 
     public BasicEngineRunnable(State initialState, Storage storage, Algorithm algorithm) {
         this.currentState = initialState;
@@ -31,12 +33,37 @@ public class BasicEngineRunnable implements Runnable {
     }
 
     private State computeNewState(State oldState) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            // we have to pass interruption event, since it's cleaned after catching an exception
-            Thread.currentThread().interrupt();
+
+        logger.debug("Computing new state");
+
+        int xLen = oldState.cells().length;
+        if(xLen==0){
+            logger.info("Compute new state::xLen==0");
+            return oldState;
         }
-        return oldState;
+        int yLen = oldState.cells()[0].length;
+        if(yLen==0){
+            logger.info("Compute new state::yLen==0");
+            return oldState;
+        }
+        int zLen = oldState.cells()[0][0].length;
+        if(zLen==0){
+            logger.info("Compute new state::zLen==0");
+            return oldState;
+        }
+
+        State newState = new State(new Cell[xLen][yLen][zLen]);
+
+        for(int x=0;x<xLen;x++){
+            for(int y=0;y<yLen;y++){
+                for(int z=0;z<zLen;z++){
+                    newState.cells()[x][y][z] = algorithm.compute(oldState, new Index(x, y, z));
+                }
+            }
+        }
+
+        logger.debug("Computed new state");
+
+        return newState;
     }
 }
--- src/main/java/pl/edu/agh/firecell/engine/algorithm/BasicAlgorithm.java
+++ src/main/java/pl/edu/agh/firecell/engine/algorithm/BasicAlgorithm.java
@@ -4,10 +4,53 @@ import pl.edu.agh.firecell.model.Cell;
 import pl.edu.agh.firecell.model.Index;
 import pl.edu.agh.firecell.model.State;
 
+import java.util.List;
+
 public class BasicAlgorithm implements Algorithm {
 
     @Override
     public Cell compute(State oldState, Index cellIndex) {
-        return new Cell(0);
+
+        Cell oldCell = oldState.getCell(cellIndex);
+
+        double temp1 = conductivity(
+                oldState.getCell(cellIndex.getNorthIndex()),
+                oldCell,
+                oldState.getCell(cellIndex.getSouthIndex()));
+
+        double temp2 = conductivity(
+                oldState.getCell(cellIndex.getDownIndex()),
+                oldCell,
+                oldState.getCell(cellIndex.getUpIndex()));
+
+        double temp3 = conductivity(
+                oldState.getCell(cellIndex.getEastIndex()),
+                oldCell,
+                oldState.getCell(cellIndex.getWestIndex()));
+
+        return new Cell(avg(List.of(temp1, temp2, temp3)), oldCell.coefficientConductivity());
     }
+
+    private double conductivity(Cell left, Cell middle, Cell right){
+
+        double dt = 0.05;
+        double gammaPrimN = 0;
+        double gammaPrimN1 = 0;
+
+        return middle.temp() +
+                (
+                        gammaPrimN  * (middle.temp() - left.temp())   -
+                        gammaPrimN1 * (right.temp()  - middle.temp())
+                ) * dt;
+
+    }
+
+    private double avg(List<Double> arr){
+        Double r = (double) 0;
+        for (Double aDouble : arr) {
+            r += aDouble;
+        }
+        return r/arr.size();
+    }
+
 }
