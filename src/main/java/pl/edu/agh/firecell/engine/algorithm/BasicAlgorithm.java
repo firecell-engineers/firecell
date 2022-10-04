b9527372f8f4c436ce85b9cacc9727c7e4b332f2
Kamil <61463685+K4m1lP@users.noreply.github.com>
2022-10-04 16:24:30 +0200

    Air temperature propagation improvement including letter T rule

--- src/main/java/pl/edu/agh/firecell/engine/algorithm/BasicAlgorithm.java
+++ src/main/java/pl/edu/agh/firecell/engine/algorithm/BasicAlgorithm.java
@@ -13,15 +13,15 @@ import pl.edu.agh.firecell.model.util.NeighbourUtils;
 public class BasicAlgorithm implements Algorithm {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final double deltaTime;
-    public static final double CONVECTION_COEFFICIENT = 1.25;
+    private final TemperatureCalculation temperatureCalculation;
+    public static final double CONVECTION_COEFFICIENT = 1;
     // should be dependent on the material in the future
-    public static final double CONDUCTIVITY_COEFFICIENT = 0.02;
-    public static final int MAX_BURNING_TIME = 5;
-    public static final double MIN_BURNING_TEMPERATURE = 100;
+    public static final double CONDUCTIVITY_COEFFICIENT = 0.2;
+    public static final int MAX_BURNING_TIME = 50;
+
 
     public BasicAlgorithm(double deltaTime) {
-        this.deltaTime = deltaTime;
+        this.temperatureCalculation = new TemperatureCalculation(deltaTime);
     }
 
     @Override
@@ -29,57 +29,97 @@ public class BasicAlgorithm implements Algorithm {
 
         Cell oldCell = oldState.getCell(cellIndex);
 
-        // temperature propagation
-        double newTemperature = computeNewTemperature(oldState, cellIndex, oldCell);
-
-        // fire propagation
+        double newTemperature = temperatureCalculation.computeNewTemperature(oldState, cellIndex, oldCell);
         boolean newFlammable = oldCell.flammable();
         int newBurningTime = oldCell.burningTime();
-        if (oldCell.flammable() && oldCell.burningTime() >= 0 && newTemperature > MIN_BURNING_TEMPERATURE) {
-            newBurningTime++;
+        int newRemainingHeightOfFirePillar = oldCell.remainingHeightOfFirePillar();
+        boolean newPossibleToGoUp = oldCell.possibleToGoUp();
+        boolean isBurned = false;
+
+        newPossibleToGoUp = isPossibleToGoUp(oldState, cellIndex, newPossibleToGoUp);
+
+        // wood section
+        if(oldCell.material().equals(Material.WOOD)) {
+            //set wood on fire
+            if (newBurningTime == 0 && newTemperature > 250 && MAX_BURNING_TIME != 0) {
+                newBurningTime++;
+                newTemperature = 550;
+            }
+            //keep wood on fire
+            if (newBurningTime > 0 && newBurningTime <= MAX_BURNING_TIME) {
+                newBurningTime++;
+                newTemperature = Math.max(550, newTemperature);
+            }
+            //wood is carbonated
+            if (newBurningTime > MAX_BURNING_TIME) {
+                isBurned = true;
+                newFlammable = false;
+            }
         }
-        if (newBurningTime > MAX_BURNING_TIME) {
-            newFlammable = false;
+
+        // air section
+        if(oldCell.material().equals(Material.AIR)){
+            try {
+                Cell underCell = oldState.getCell(NeighbourUtils.down(cellIndex));
+                if (underCell.flammable() && underCell.burningTime() > 0) {
+                    newRemainingHeightOfFirePillar = underCell.remainingHeightOfFirePillar() - 1;
+                    if (newRemainingHeightOfFirePillar > 0) {
+                        // Rethink it
+                        newTemperature = Math.max(newTemperature, 600);
+                        newBurningTime++;
+                    }
+                } else {
+                    Cell neighbourOnFire = doesNeighbourOnFire(oldState, cellIndex);
+                    if (neighbourOnFire!=null && !neighbourOnFire.possibleToGoUp()) {
+                        newRemainingHeightOfFirePillar = neighbourOnFire.remainingHeightOfFirePillar() - 1;
+                        if (newRemainingHeightOfFirePillar > 0) {
+                            newTemperature = Math.max(newTemperature, 600);
+                            newBurningTime++;
+                        }
+                    }
+                }
+            }catch (IndexOutOfBoundsException ignored){}
         }
 
         return new Cell(
                 newTemperature,
                 newBurningTime,
                 newFlammable,
-                oldCell.material()
+                oldCell.material(),
+                newRemainingHeightOfFirePillar,
+                newPossibleToGoUp
         );
     }
 
-    private double computeNewTemperature(State oldState, Vector3i cellIndex, Cell oldCell) {
-        return oldCell.temperature() + deltaTime *
-                switch (oldCell.material().getMatterState()) {
-                    case SOLID -> computeConduction(oldState, oldCell, cellIndex);
-                    case FLUID -> computeConvection(oldState, oldCell, cellIndex);
-                };
+    private boolean isPossibleToGoUp(State oldState, Vector3i cellIndex, boolean newPossibleToGoUp) {
+        try {
+            if (oldState.getCell(NeighbourUtils.up(cellIndex)).material().getMatterState().equals(MatterState.SOLID)) {
+                newPossibleToGoUp = false;
+            }
+        } catch (IndexOutOfBoundsException ignored){
+            newPossibleToGoUp = false;
+        }
+        return newPossibleToGoUp;
     }
 
-    private double computeConduction(State oldState, Cell oldCell, Vector3i cellIndex) {
-        return NeighbourUtils.neighboursStream(cellIndex)
-                .filter(oldState::hasCell)
-                .map(oldState::getCell)
-                .filter(Cell::isSolid)
-                .mapToDouble(neighbour -> computeConductivityWithNeighbour(oldCell, neighbour))
-                .sum();
+    private Cell doesNeighbourOnFire(State oldState, Vector3i cellIndex){
+        Cell northCell = oldState.getCell(NeighbourUtils.north(cellIndex));
+        Cell southCell = oldState.getCell(NeighbourUtils.south(cellIndex));
+        Cell westCell = oldState.getCell(NeighbourUtils.west(cellIndex));
+        Cell eastCell = oldState.getCell(NeighbourUtils.east(cellIndex));
+        if(doesHorizontalFire(northCell))
+            return northCell;
+        if(doesHorizontalFire(southCell))
+            return southCell;
+        if(doesHorizontalFire(westCell))
+            return westCell;
+        if(doesHorizontalFire(eastCell))
+            return eastCell;
+        return null;
     }
 
-    private double computeConvection(State oldState, Cell oldCell, Vector3i cellIndex) {
-        return NeighbourUtils.neighboursStream(cellIndex, NeighbourUtils.Axis.Y)
-                .filter(oldState::hasCell)
-                .map(oldState::getCell)
-                .mapToDouble(neighbourCell -> computeConvectionWithNeighbour(oldCell, neighbourCell))
-                .sum();
+    private boolean doesHorizontalFire(Cell cell){
+        return cell.burningTime() > 0 && cell.remainingHeightOfFirePillar() > 0 && !cell.possibleToGoUp();
     }
 
-    private static double computeConductivityWithNeighbour(Cell oldCell, Cell neighbour) {
-        return CONDUCTIVITY_COEFFICIENT * (oldCell.temperature() - neighbour.temperature());
-    }
-
-    private static double computeConvectionWithNeighbour(Cell oldCell, Cell neighbour) {
-        return CONVECTION_COEFFICIENT * (oldCell.temperature() - neighbour.temperature());
-    }
 }
---
+++ src/main/java/pl/edu/agh/firecell/engine/algorithm/TemperatureCalculation.java
@@ -0,0 +1,176 @@
+package pl.edu.agh.firecell.engine.algorithm;
+
+import org.joml.Vector3i;
+import pl.edu.agh.firecell.model.Cell;
+import pl.edu.agh.firecell.model.Material;
+import pl.edu.agh.firecell.model.MatterState;
+import pl.edu.agh.firecell.model.State;
+import pl.edu.agh.firecell.model.util.NeighbourUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static pl.edu.agh.firecell.engine.algorithm.BasicAlgorithm.CONDUCTIVITY_COEFFICIENT;
+import static pl.edu.agh.firecell.engine.algorithm.BasicAlgorithm.CONVECTION_COEFFICIENT;
+
+public class TemperatureCalculation {
+
+    private final double deltaTime;
+
+    public TemperatureCalculation(double deltaTime){
+        this.deltaTime = deltaTime;
+    }
+
+    public double computeNewTemperature(State oldState, Vector3i cellIndex, Cell oldCell) {
+        return oldCell.temperature() + deltaTime *
+                switch (oldCell.material().getMatterState()) {
+                    case SOLID -> computeConduction(oldState, oldCell, cellIndex);
+                    case FLUID -> computeConvection(oldState, oldCell, cellIndex);
+        };
+    }
+
+
+    //    private double computeConduction(State oldState, Cell oldCell, Vector3i cellIndex) {
+    //        return NeighbourUtils.neighboursStream(cellIndex)
+    //                .filter(oldState::hasCell)
+    //                .map(oldState::getCell)
+    //                .filter(Cell::isSolid)
+    //                .mapToDouble(neighbour -> computeConductivityWithNeighbour(oldCell, neighbour))
+    //                .sum();
+    //    }
+
+
+    private double computeConduction(State oldState, Cell oldCell, Vector3i cellIndex) {
+        double yTemp = 0;
+        double zTemp = 0;
+        double xTemp = 0;
+
+        try {
+            Cell northCell = oldState.getCell(NeighbourUtils.north(cellIndex));
+            MatterState northMatter = northCell.material().getMatterState();
+            Cell southCell = oldState.getCell(NeighbourUtils.south(cellIndex));
+            MatterState southMatter = southCell.material().getMatterState();
+
+            Cell upCell = oldState.getCell(NeighbourUtils.up(cellIndex));
+            MatterState upMatter = upCell.material().getMatterState();
+            Cell downCell = oldState.getCell(NeighbourUtils.down(cellIndex));
+            MatterState downMatter = downCell.material().getMatterState();
+
+            Cell eastCell = oldState.getCell(NeighbourUtils.east(cellIndex));
+            MatterState eastMatter = eastCell.material().getMatterState();
+            Cell westCell = oldState.getCell(NeighbourUtils.west(cellIndex));
+            MatterState westMatter = westCell.material().getMatterState();
+
+            if (northMatter == MatterState.SOLID && southMatter == MatterState.SOLID)
+                yTemp = computeConductivity(northCell, oldCell, southCell);
+            else if (northMatter == MatterState.SOLID)
+                yTemp = computeConductivity(northCell, oldCell, oldCell.getCopy());
+            else if (southMatter == MatterState.SOLID)
+                yTemp = computeConductivity(oldCell.getCopy(), oldCell, southCell);
+
+            if (upMatter == MatterState.SOLID && downMatter == MatterState.SOLID)
+                zTemp = computeConductivity(upCell, oldCell, downCell);
+            else if (upMatter == MatterState.SOLID)
+                zTemp = computeConductivity(upCell, oldCell, oldCell.getCopy());
+            else if (downMatter == MatterState.SOLID)
+                zTemp = computeConductivity(oldCell.getCopy(), oldCell, downCell);
+
+            if (eastMatter == MatterState.SOLID && westMatter == MatterState.SOLID)
+                xTemp = computeConductivity(eastCell, oldCell, westCell);
+            else if (eastMatter == MatterState.SOLID)
+                xTemp = computeConductivity(eastCell, oldCell, oldCell.getCopy());
+            else if (westMatter == MatterState.SOLID)
+                xTemp = computeConductivity(oldCell.getCopy(), oldCell, westCell);
+
+        } catch (IndexOutOfBoundsException ignored) {
+            if (oldState.hasCell(NeighbourUtils.up(cellIndex))){
+                zTemp = computeConductivity(oldState.getCell(NeighbourUtils.up(cellIndex)), oldCell, oldCell.getCopy());
+            }
+        }
+        return yTemp + zTemp + xTemp;
+    }
+
+    private double computeConductivity(Cell former, Cell middle, Cell latter) {
+        return - CONDUCTIVITY_COEFFICIENT * (tempDiff(middle, former) + tempDiff(middle, latter));
+    }
+
+    private static double tempDiffAbs(Cell cellOne, Cell cellTwo) {
+        return Math.abs(tempDiff(cellOne, cellTwo));
+    }
+
+    private static double tempDiff(Cell cellOne, Cell cellTwo) {
+        return cellOne.temperature() - cellTwo.temperature();
+    }
+
+    private double computeConvection(State oldState, Cell oldCell, Vector3i cellIndex) {
+
+        final int HOTTER = 0;
+        final int COLDER = 1;
+
+        double fromDownToMe = 0;
+        double fromMeToUp = 0;
+
+        double fromMeToNeighbour = 0;
+        double toMeFromNeighbour = 0;
+
+        try {
+            Cell cellUnder = oldState.getCell(NeighbourUtils.down(cellIndex));
+            if (cellUnder.material().equals(Material.AIR) && cellUnder.temperature() > oldCell.temperature()) {
+                fromDownToMe += CONVECTION_COEFFICIENT * tempDiffAbs(oldCell, cellUnder);
+            }
+        } catch (IndexOutOfBoundsException ignored) {}
+
+        try {
+            Cell cellAbove = oldState.getCell(NeighbourUtils.up(cellIndex));
+            if (cellAbove.material().equals(Material.AIR)) {
+                if(cellAbove.temperature() < oldCell.temperature()) {
+                    fromMeToUp -= CONVECTION_COEFFICIENT * tempDiffAbs(oldCell, cellAbove);
+                } else {
+                    // cell above me is hotter
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {}
+
+        if (!oldState.hasCell(NeighbourUtils.up(cellIndex)) || oldState.getCell(NeighbourUtils.up(cellIndex)).temperature() > 100) {
+            // cell above me is ceiling
+            try {
+                Optional<Cell> northCell = oldState.hasCell(NeighbourUtils.north(cellIndex)) ? Optional.of(oldState.getCell(NeighbourUtils.north(cellIndex))) : Optional.empty();
+                Optional<Cell> southCell = oldState.hasCell(NeighbourUtils.south(cellIndex)) ? Optional.of(oldState.getCell(NeighbourUtils.south(cellIndex))) : Optional.empty();
+                Optional<Cell> eastCell = oldState.hasCell(NeighbourUtils.east(cellIndex)) ? Optional.of(oldState.getCell(NeighbourUtils.east(cellIndex))) : Optional.empty();
+                Optional<Cell> westCell = oldState.hasCell(NeighbourUtils.west(cellIndex)) ? Optional.of(oldState.getCell(NeighbourUtils.west(cellIndex))) : Optional.empty();
+
+                List<Optional<Cell>> neighbours = new ArrayList<>(List.of(northCell, southCell, eastCell, westCell));
+                final int[] counter = {0, 0};
+
+                neighbours.stream()
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .forEach(cell -> {
+                            if (cell.temperature() > oldCell.temperature())
+                                counter[HOTTER]++;
+                            else
+                                counter[COLDER]++;
+                        });
+
+                // give to other
+                fromMeToNeighbour -= neighbours.stream()
+                        .filter(Optional::isPresent)
+                        .filter(cell -> cell.get().temperature() < oldCell.temperature())
+                        .mapToDouble(cell -> tempDiffAbs(oldCell, cell.get()) / counter[COLDER])
+                        .sum();
+
+                // take from other
+                fromMeToNeighbour += neighbours.stream()
+                        .filter(Optional::isPresent)
+                        .filter(cell -> cell.get().temperature() > oldCell.temperature())
+                        .mapToDouble(cell -> tempDiffAbs(oldCell, cell.get()) / counter[HOTTER])
+                        .sum();
+
+            } catch (IndexOutOfBoundsException ignored){}
+        }
+
+        return fromDownToMe + fromMeToUp + fromMeToNeighbour + toMeFromNeighbour;
+    }
+
+}
